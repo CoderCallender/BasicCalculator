e9f232955352561d81402af842be0a7d42e466a1
Chase Callender <chase.callender@thinksmartbox.com>
2018-08-21 13:59:34 +0100

    start of video 2
    worked up to 20 mins on the video

--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -26,9 +26,16 @@ namespace BasicCalculator
         #endregion
 
         #region Operators
+        /// <summary>
+        /// Clears the user input text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
         private void CEButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello!");
+            this.UserInputText.Text = String.Empty; //Clear the text from the user input
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void DELButton_Click(object sender, EventArgs e)
@@ -38,28 +45,34 @@ namespace BasicCalculator
 
         private void DivButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("/");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void MultButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("*");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void MinusButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("-");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void AddButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("+");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void EqualsButton_Click(object sender, EventArgs e)
-        {
-
+        { 
+            CalculateEquation();
         }
+
+
 
         #endregion
 
@@ -67,59 +80,94 @@ namespace BasicCalculator
 
         private void PeriodButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue(".");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void ZeroButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("0");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void OneButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("1");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void TwoButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("2");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void ThreeButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("3");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void FourButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("4");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void FiveButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("5");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void SixButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("6");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void SevenButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("7");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void EightButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("8");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
         private void NineButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("9");
+            FocusInputText();   //put the cursor back to the user input box
         }
 
+        #endregion
+
+        /// <summary>
+        /// Calculates the given equation and outputs the answer to the label
+        /// </summary>
+        private void CalculateEquation()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Focus the user input text
+        /// </summary>
+        private void FocusInputText()
+        {
+            this.UserInputText.Focus();
+        }
+
+        private void InsertTextValue(string Value)
+        {
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, Value);
+        }
         #endregion
     }
 }
